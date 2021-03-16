6f11e7c31690091cfffbf822080dd9d45691593f
林金杰 <1755684077@qq.com>
2021-03-16 16:49:08 +0800

    图片打包+html打包

--- 02_webpack初体验/src/index.js
+++ 02_webpack初体验/src/index.js
@@ -5,9 +5,9 @@
  *  开发环境：webpack ./src/index.js -o ./build/built.js --mode=development
  *    webpack会以 ./src/index.js 为入口文件开始打包，打包后输入到 ./build/built.js
  *    整体打包环境，是开发环境
- *  生成环境：webpack ./src/index.js -o ./build/built.js --mode=production
+ *  生产环境：webpack ./src/index.js -o ./build/built.js --mode=production
  *    webpack会以 ./src/index.js 为入口文件开始打包，打包后输入到 ./build/built.js
- *    整体打包环境，是生产环境
+ *      
  * 
  * 2. 结论：
  *   1. webpack能处理js/json资源. 不能处理css/img等其他资源
